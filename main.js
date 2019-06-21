b07656958edfb8f3c71333bc63695cc30ada0dc7
Rodrigo Golfeto <rgolfeto@gmail.com>
2019-06-21 00:13:40 -0400

    Utilizando Axios

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-var minhaPromisse = function() {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET','https://api.github.com/users/rodrigogolfeto');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-minhaPromisse()
+axios.get('https://api.github.com/users/rodrigogolfeto')
     .then(function(response) {
-        console.log(response);
+        console.log(response.data.avatar_url);
     })
     .catch(function(error) {
         console.warn(error);
